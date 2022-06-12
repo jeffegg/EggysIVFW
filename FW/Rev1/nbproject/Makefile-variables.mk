2a671520d432095a3362378f2fa9b45901fa7ed2
Jeff Eglinger <jeffeggy@gmail.com>
2022-06-11 22:35:29 -0700

    fw

---
+++ FW/Rev1/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Rev1.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Rev1.production.hex
