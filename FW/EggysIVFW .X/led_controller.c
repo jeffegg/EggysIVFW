d5e6671f3efca9969370075d19290e4306906c11
Jeff Eglinger <jeffeggy@gmail.com>
2023-03-18 01:37:11 -0700

    Adding Led Controller and licenses for all files

---
+++ FW/EggysIVFW .X/led_controller.c
@@ -0,0 +1,250 @@
+/*
+    EggysIVFW - Custom Firmware for Pentair's Intellivalve (TM)
+    Copyright (C) 2021-2023  Jeff "Eggy" Eglinger
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ 
+ led_controller.c: Implementation of the led_controller for the valve.
+ */
+
+#include <xc.h>
+#include <stdbool.h>
+#include "led_controller.h"
+
+// 0 -> AUTO
+// 1 -> OFF
+// 2 -> 10
+// 3 -> 8
+// 4 -> 6
+// 5 -> 4
+// 6 -> 2
+// 7 -> 0
+// 8 -> 24
+// 9 -> 22
+// 10 -> 20
+// 11 -> 18
+// 12 -> 16
+// 13 -> 14
+// 14 -> 12
+// 15 -> Service
+
+volatile LEDS display;
+volatile LEDS next_display;
+
+void ControlLights(LEDS *param_0)
+{
+    uint8_t delayLoop1 = 0;
+    uint8_t delayLoop2 = 0;
+    uint16_t ledValue = param_0->raw_leds;
+    
+    SSP1CON1 = 8;
+    RC3PPS = RC3PPS & 0xe0;
+    RC4PPS = RC4PPS & 0xe0;
+    SSPCLKPPS = 0;
+    SSPDATPPS = 0;
+    TRISC &= 0xef;
+    TRISC &= 0xf7;
+    LATC &= 0xf7;
+
+    delayLoop1 = 6;
+    delayLoop2 = 0x30;
+    do {
+        do {
+            delayLoop2 -= 1;
+        } while (delayLoop2 != 0);
+        delayLoop1 -= 1;
+    } while (delayLoop1 != 0);
+
+    for (uint8_t i = 0; i < 0x10; i += 1) {
+        if ((( ledValue >> i) & 0x1) == 0) {
+            LATC &= 0xef;
+        }
+        else {
+            LATC |= 0x10;
+        }
+
+        delayLoop1 = 0x1a;
+        do {
+            delayLoop1 -= 1;
+        } while (delayLoop1 != 0);
+
+        LATC |= 8;
+
+        delayLoop1 = 6;
+        delayLoop2 = 0x30;
+        do {
+            do {
+                delayLoop2 -= 1;
+            } while (delayLoop2 != 0);
+            delayLoop1 -= 1;
+        } while (delayLoop1 != 0);
+
+        LATC &= 0xf7;
+    }
+
+    LATC &= 0xef;
+    LATC |= 0x20;
+
+    delayLoop1 = 0x1a;
+    do {
+        delayLoop1 -= 1;
+    } while (delayLoop1 != 0);
+
+    LATC &= 0xdf;
+    RC3PPS = (RC3PPS & 0xe0) | 0x10;
+    RC4PPS = (RC4PPS & 0xe0) | 0x11;
+    SSPCLKPPS = 0x13;
+    SSPDATPPS = 0x14;
+
+    TRISC |= 0x10;
+    TRISC |= 8;
+    SSP1CON1 = 0x28;
+
+    return;
+}
+
+void SetLeds(void)
+{
+    uint8_t valveLocation = GetCurrentValveLocation();
+    uint8_t ledToLight = valveLocation >> 2;
+    uint8_t between2Lights = valveLocation & 0x1;
+    
+    if (nextValveMode == VALVE_MODE_NORMAL)
+        next_display.LEDbits.AUTO_LED = 1;
+    else
+        next_display.LEDbits.AUTO_LED = 0;
+
+    if (nextValveMode == VALVE_MODE_MAINTAINENCE)
+        next_display.LEDbits.SERVICE_LED = 1;
+    else
+        next_display.LEDbits.SERVICE_LED = 0;
+
+    if (nextValveMode == VALVE_MODE_SETTINGS)
+        BlueModeLed_SetLow();
+    else
+        BlueModeLed_SetHigh();
+    
+    // If we are in remote mode (look only at remote bit since we keep track of the last setting)
+    if ((nextValveMode & VALVE_MODE_REMOTE) == VALVE_MODE_REMOTE)
+    {
+        next_display.LEDbits.AUTO_LED = 1;
+        next_display.LEDbits.SERVICE_LED = 0;
+        BlueModeLed_SetLow();
+    }
+    
+    switch(valveLocation)
+    {
+        case 0:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED0 = 1; 
+            break;
+        case 2:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED2 = 1; 
+            break;
+        case 4:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED4 = 1; 
+            break;
+        case 6:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED6 = 1;     
+            break;
+        case 8:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED8 = 1; 
+            break;
+        case 10:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED10 = 1; 
+            break;
+        case 12:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED12 = 1; 
+            break;
+        case 14:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED14 = 1;     
+            break;
+        case 16:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED16 = 1; 
+            break;
+        case 18:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED18 = 1; 
+            break;
+        case 20:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED20 = 1; 
+            break;
+        case 22:
+            if (!between2Lights)
+            {
+                next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+            }
+            next_display.LEDbits.LED22 = 1;   
+            break;            
+        case 24:
+         if (!between2Lights)
+         {
+             next_display.raw_leds = next_display.raw_leds & 0x7FFC;
+         }
+         next_display.LEDbits.LED22 = 1;  
+         break;
+    }
+}
+
+void SetBlueModeLed(bool led_on)
+{
+  if (led_on != false)
+      LATA &= 0x7f;
+  else
+      LATA |= 0x80;
+  return;
+}
+
--- FW/EggysIVFW .X/nbproject/Makefile-default.mk
+++ FW/EggysIVFW .X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c led_controller.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/led_controller.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/led_controller.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/led_controller.p1
 
 # Source Files
-SOURCEFILES=main.c
+SOURCEFILES=main.c led_controller.c
 
 
 
@@ -102,6 +102,14 @@ ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/led_controller.p1: led_controller.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/led_controller.p1.d 
+	@${RM} ${OBJECTDIR}/led_controller.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=icd4   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/led_controller.p1 led_controller.c 
+	@-${MV} ${OBJECTDIR}/led_controller.d ${OBJECTDIR}/led_controller.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/led_controller.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
@@ -110,6 +118,14 @@ ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
 	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/led_controller.p1: led_controller.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/led_controller.p1.d 
+	@${RM} ${OBJECTDIR}/led_controller.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/led_controller.p1 led_controller.c 
+	@-${MV} ${OBJECTDIR}/led_controller.d ${OBJECTDIR}/led_controller.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/led_controller.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
